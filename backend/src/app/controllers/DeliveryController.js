f3974d371dd626b1b933d39bdebd03e5565b311a
rodrigosakamoto <rodosakamoto@gmail.com>
2020-02-17 09:48:52 -0300

    Delivery CRUD

---
+++ backend/src/app/controllers/DeliveryController.js
@@ -0,0 +1,136 @@
+import * as Yup from 'yup';
+
+import Delivery from '../models/Delivery';
+import File from '../models/File';
+import Deliveryman from '../models/Deliveryman';
+import Recipient from '../models/Recipient';
+
+class DeliveryController {
+  // Listar entregas
+  async index(req, res) {
+    const deliveries = await Delivery.findAll({
+      attributes: ['id', 'product', 'canceled_at', 'start_date', 'end_date'],
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: [
+            'name',
+            'street',
+            'number',
+            'complement',
+            'state',
+            'city',
+            'zipcode',
+          ],
+        },
+        {
+          model: Deliveryman,
+          as: 'deliveryman',
+          attributes: ['id', 'name', 'email'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+    return res.json(deliveries);
+  }
+
+  // Cadastro de entregas
+  async store(req, res) {
+    // Validação dos dados de entrada
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number().required(),
+      deliveryman_id: Yup.number().required(),
+      product: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { recipient_id, deliveryman_id, product } = req.body;
+
+    const recipient = await Recipient.findByPk(recipient_id);
+    const deliveryman = await Deliveryman.findByPk(deliveryman_id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exists' });
+    }
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman does not exists' });
+    }
+
+    const { id } = await Delivery.create({
+      recipient_id,
+      deliveryman_id,
+      product,
+    });
+
+    return res.json({
+      id,
+      recipient_id,
+      deliveryman_id,
+      product,
+    });
+  }
+
+  async update(req, res) {
+    const delivery = await Delivery.findByPk(req.params.id);
+
+    if (!delivery) {
+      return res.status(400).json({ error: 'Delivery does not exixsts' });
+    }
+
+    const { recipient_id, deliveryman_id, product } = req.body;
+
+    const recipient = await Recipient.findByPk(recipient_id);
+    const deliveryman = await Deliveryman.findByPk(deliveryman_id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exists' });
+    }
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman does not exists' });
+    }
+
+    const { id, start_date, end_date } = await delivery.update(req.body);
+
+    return res.json({
+      id,
+      recipient_id,
+      deliveryman_id,
+      product,
+      start_date,
+      end_date,
+    });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const delivery = await Delivery.findByPk(id);
+
+    if (!delivery) {
+      return res.status(400).json({ error: 'Delivery does not exists' });
+    }
+
+    await Delivery.destroy({ where: { id } });
+
+    return res.json({ message: 'Successful deleted.' });
+  }
+}
+
+export default new DeliveryController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import DeliveryController from './app/controllers/DeliveryController';
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -25,6 +26,11 @@ routes.get('/deliverymans', DeliverymanController.index);
 routes.post('/deliverymans', DeliverymanController.store);
 routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
+
+routes.get('/deliveries', DeliveryController.index);
+routes.post('/deliveries', DeliveryController.store);
+routes.put('/deliveries/:id', DeliveryController.update);
+routes.delete('/deliveries/:id', DeliveryController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
