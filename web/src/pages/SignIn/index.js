1373b86c7306f8acd864abda8f29ed0f99aa7fdf
rodrigosakamoto <rodosakamoto@gmail.com>
2020-03-16 13:29:18 -0300

    config store

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/fastfeet-logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
-  return <h1>Ola</h1>;
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <strong>SEU E-MAIL</strong>
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
+        <strong>SUA SENHA</strong>
+        <Input name="password" type="password" placeholder="*************" />
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
--- web/src/pages/_layouts/auth/styles.js
+++ web/src/pages/_layouts/auth/styles.js
@@ -1,8 +1,65 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: #7d40e7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  width: 100%;
+  max-width: 360px;
+  background: #fff;
+  padding: 60px 30px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    strong {
+      margin-bottom: 10px;
+      font-size: 14px;
+      font-weight: bold;
+      color: #444;
+      text-align: left;
+    }
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      padding: 12px 15px;
+      margin-bottom: 15px;
+      font-size: 16px;
+
+      &::placeholder {
+        color: #999;
+      }
+    }
+
+    span {
+      color: #fb6f91;
+      align-self: flex-start;
+      font-weight: bold;
+      margin: 0 0 15px;
+    }
+
+    button {
+      background: #7d40e7;
+      border: 0;
+      border-radius: 4px;
+      color: #fff;
+      font-size: 16px;
+      font-weight: bold;
+      padding: 12px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#7d40e7')};
+      }
+    }
+  }
+`;
